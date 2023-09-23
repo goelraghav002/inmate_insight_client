82042fb6d3a897e175f6b4998c76f1c270d2ba0d
Akshansh <akshanshmodi2002@gmail.com>
2023-09-23 15:27:04 +0530

    hidden rm

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 const Courses = () => {
   return (
     <div className="relative z-0 bg-gradient-to-l from-stone-200 via-lime-100 to-stone-200  min-h-screen p-4 flex transition-all duration-300">
-      <div className="hidden sm:flex relative mr-4">
+      <div className="sm:flex relative mr-4">
         <Sidebar />
       </div>
 
