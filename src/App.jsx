3a52ec02f6ae28382fa3e887883fa171b9fd7de7
Akshansh <akshanshmodi2002@gmail.com>
2023-09-29 00:05:08 +0530

    home🤘

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import Courses from './pages/Courses'
-import Course from './pages/Course'
-import Profile from './pages/profile/Profile'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Courses from "./pages/Courses";
+import Course from "./pages/Course";
+import Profile from "./pages/profile/Profile";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -16,7 +15,7 @@ function App() {
       <Route path="/profile" element={<Profile />} />
       <Route path="/chat" element={<Home />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
